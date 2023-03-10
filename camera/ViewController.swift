24a3c6613d929e943c06b6e4eef8a2c6c39905ac
charrrleee <charlcpy@gmail.com>
2023-03-10 16:02:21 -0500

    Initial Commit

---
+++ camera/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  camera
+//
+//  Created by Charlene Cheung on 10/3/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
